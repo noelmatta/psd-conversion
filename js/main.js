2969a21a475b4f8a304f1d7c6c6f1723dcefc082
noelmatta <noelmatta82@gmail.com>
2021-06-17 18:26:17 -0400

    Added functionality to Testimonial thumbnails and Project Gallery section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
-// Global variables
-const testimonials = Array.from(document.querySelectorAll('.testimonial-content'));
-const testHeadshots = Array.from(document.querySelectorAll('.headshot'));
 
 // Functions
-changeTestimonial = (direction) => {
-  const activeTestimonial = document.querySelector('.testimonial-content.active');
-  let activeIndex = testimonials.indexOf(activeTestimonial);
+prevNextSlide = (selection, slideType) => {
 
-  (direction === 'prev') ?
+  const slides = Array.from(document.querySelectorAll(slideType));
+
+  const activeSlide = document.querySelector(`${slideType}.active`);
+  let activeIndex = slides.indexOf(activeSlide);
+
+  (selection === 'prev') ?
     (activeIndex == 0) ?
-      activeIndex = testimonials.length - 1 : activeIndex-- :
-    (activeIndex == testimonials.length - 1) ?
+      activeIndex = slides.length - 1 : activeIndex-- :
+    (activeIndex == slides.length - 1) ?
       activeIndex = 0 : activeIndex++;  
 
-  testimonials.forEach((testimonial) => {
-    testimonial.classList.remove('active');
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
   });
 
-  testimonials[activeIndex].classList.add('active');
+  slides[activeIndex].classList.add('active');
+}
+
+setActive = (selected) => {  
+
+  const thumbClass = '.' + selected.classList[0];
+  const thumbs = Array.from(document.querySelectorAll(thumbClass));
+  
+  const sliderType = '.' + document.querySelector(thumbClass).parentNode.id;
+  const slides = Array.from(document.querySelectorAll(sliderType));
+  const sliderID = '.' + selected.dataset.name;
+
+  thumbs.forEach((thumb) => {
+    thumb.classList.remove('active');
+  });  
+  
+  selected.classList.add('active');
+
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+
+  document.querySelector(sliderID+sliderType).classList.add('active');
+  
 }
 
